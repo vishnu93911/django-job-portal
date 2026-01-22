83a9c8ecddb1ef69f4d047f471fae67493eb6990
vishnu93911 <vishhnu040@gmail.com>
2026-01-22 13:25:40 +0530

    changed allow hists name

--- jobfinder/settings.py
+++ jobfinder/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-896ac0lpwar%^2-2^x&hy_k@%jbl21=+v7y5p0&j@@7&tmy9nt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-job-portal-3.onrender.com',"127.0.0.1","localhost","127.0.0.1:8000"]
+ALLOWED_HOSTS = ['django-job-portal-6.onrender.com',"127.0.0.1","localhost","127.0.0.1:8000"]
 
 
 # Application definition
